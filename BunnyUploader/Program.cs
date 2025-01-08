e73dc71fe7f9e2fa9b96b25926ee52d67f28b472
Brian Gish <gish.brian@gmail.com>
2025-01-07 22:24:40 -0800

    Adding new command line app BunnyUploader for uploading folders to BunnyCDN
    - Also contains a NativeAOT publish to Folder profile so we have a single executable for running this app

---
+++ BunnyUploader/Program.cs
@@ -0,0 +1,12 @@
+﻿
+using ConsoleAppFramework;
+
+var consoleApp = ConsoleApp.Create();
+
+consoleApp.Add("", (string storageZone, string apiAccessKey, string mainReplicationRegion, string directory) =>
+{
+    var storage = new BunnyCDN.Net.Storage.BunnyCDNStorage(storageZone, apiAccessKey, mainReplicationRegion);
+    storage.UploadLocalDirectory(directory).Wait();
+});
+
+consoleApp.Run(args);
